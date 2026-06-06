ec1ce92e0f5f2809e5f46689c7b7ac0e7c9e1dd4
NirajTharu <nirajc279@gmail.com>
2026-06-06 13:23:28 +0545

    Some changes in the first file of Swing

--- firstSwing.java
+++ firstSwing.java
@@ -1,13 +1,25 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class firstSwing {
     public static void main(String[] args) {
         
         JFrame frame = new JFrame();
+        // This helps to set the window name
         frame.setTitle("Hi there");
+        // This will close the frame when you exit the window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // This will helps to resize the window
         frame.setResizable(false);
+        // This helps to change the color of the window
+        frame.getContentPane().setBackground(new Color(12,33,22));
+        // This will help to set the size of the window
         frame.setSize(400,400);
+        // This will help to change the icon of image
+        ImageIcon image = new ImageIcon("ProfilePIc.png");
+        frame.setIconImage(image.getImage());
+        // This will make the window visible
         frame.setVisible(true);
     }
 }
