cf1d726f6962e0ac03a19604c7d08334d3f0f500
NirajTharu <nirajc279@gmail.com>
2026-06-17 19:09:45 +0545

    Adding the file of the JLayaredPane

---
+++ MyJLayeredPane.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
+public class MyJLayeredPane {
+    // JLayered Pane: Swing container that provides a third dimension for positioning components. Exampel depth, Z-index.
+    public static void main(String[] args) {
+
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED); 
+        label1.setBounds(50,50,200,200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.GREEN); 
+        label2.setBounds(100,100,200,200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.BLUE); 
+        label3.setBounds(150,150,200,200);
+    
+        JLayeredPane  layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 500, 500);
+
+        // Can be also written like this
+        // layeredPane.add(labe1,Integer.valueOf(0));
+        layeredPane.add(label1,JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(label2,JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(label3,JLayeredPane.DRAG_LAYER);
+
+        
+        JFrame frame = new JFrame("JLayred Panel");
+        frame.add(layeredPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500, 500));
+        frame.setLayout(null);
+
+
+
+        frame.setVisible(true);
+    }
+}
